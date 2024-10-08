00fda5835390c86b85bcb593e1943b60285397b3
Sachin Malik <sachmalik@gmail.com>
2024-10-08 16:07:02 +0530

    Update conform config and remove alpha

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,26 +7,16 @@ return {
 				rust = { "rustfmt" },
 				javascript = { "prettier", stop_after_first = true },
 				typescript = { "prettier", stop_after_first = true },
-				c = { "clang_format" },
-				cpp = { "clang_format" },
 				yaml = { "yamlfmt" },
 				html = { "prettier" },
 				json = { "prettier" },
 				markdown = { "prettier" },
 				sql = { "sqlfmt" },
-				asm = { "asmfmt" },
 			},
-			format_on_save = {
+			format_after_save = {
 				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
-		})
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				require("conform").format({ bufnr = args.buf })
-			end,
 		})
 	end,
 }
