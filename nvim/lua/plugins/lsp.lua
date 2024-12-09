586dce08177d10e1870e1eea3c62c38ee5144207
Sachin Malik <sachmalik@gmail.com>
2024-12-09 11:25:05 +0530

    Add hack for cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -22,6 +22,9 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
+		"hrsh7th/cmp-buffer",
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
@@ -32,6 +35,8 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local cmp_feedkeys = require("cmp.utils.feedkeys")
+			local cmp_keymap = require("cmp.utils.keymap")
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -50,14 +55,26 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = function(fallback)
+						if vim.fn.pumvisible() == 1 then
+							if vim.fn.complete_info({ "selected" }).selected == -1 then
+								cmp_feedkeys.call(cmp_keymap.t("<CR>"), "in")
+							else
+								cmp_feedkeys.call(cmp_keymap.t("<C-X><C-Z>"), "in")
+							end
+						else
+							cmp.mapping.confirm({ select = false })(fallback)
+						end
+					end,
 				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
 					{ name = "buffer" },
+					{ name = "luasnip" },
+					{ name = "nvim_lsp" },
 				}),
+				performance = {
+					max_view_entries = 20,
+				},
 			})
 		end,
 	},
@@ -65,14 +82,17 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			lspconfig.lua_ls.setup({})
 			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
 				on_attach = function(client, bufnr)
 					vim.lsp.inlay_hint.enable(true)
 				end,
 			})
 			lspconfig.pylsp.setup({
+				capabilities = capabilities,
 				settings = {
 					pylsp = {
 						plugins = {
@@ -84,6 +104,7 @@ return {
 				},
 			})
 			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
 				settings = {
 					diagnostics = {
 						ignoredCodes = { 7016, 2307 },
@@ -93,8 +114,12 @@ return {
 					},
 				},
 			})
-			lspconfig.typos_lsp.setup({})
-			lspconfig.eslint.setup({})
+			lspconfig.typos_lsp.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
