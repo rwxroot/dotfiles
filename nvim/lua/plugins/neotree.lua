7c8f17bec3fd0f0310ff6aede309ee52d83929a3
Sachin Malik <sachmalik@gmail.com>
2024-12-15 17:40:28 +0530

    Add rooter

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,32 +1,40 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
 
-		require("neo-tree").setup({
-			window = {
-				width = 40,
-			},
-			auto_clean_after_session_restore = true,
-			close_if_last_window = true,
-			filesystem = {
-				follow_current_file = {
-					enabled = true,
-					leave_dirs_open = false,
+			require("neo-tree").setup({
+				window = {
+					width = 40,
 				},
-			},
-			default_component_configs = {
-				git_status = {
-					symbols = {
-						unstaged = "",
+				auto_clean_after_session_restore = true,
+				close_if_last_window = true,
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+						leave_dirs_open = false,
 					},
 				},
-			},
-		})
-	end,
+				default_component_configs = {
+					git_status = {
+						symbols = {
+							unstaged = "",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"notjedi/nvim-rooter.lua",
+		config = function()
+			require("nvim-rooter").setup()
+		end,
+	},
 }
