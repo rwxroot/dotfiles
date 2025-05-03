4710f288d106c4bf1f29ec8cb16d3aece0e35d63
rwxroot <rwxroot@proton.me>
2025-05-03 13:07:42 +0530

    Update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,33 +1,47 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
 	config = function()
 		require("telescope").setup({
 			pickers = {
 				find_files = {
 					hidden = false,
+					theme = "dropdown"
+				},
+				live_grep = {
+					theme = "dropdown"
+				},
+				buffers = {
+					theme = "dropdown"
+				},
+				git_commits = {
+					theme = "dropdown"
+				},
+				git_status = {
+					theme = "dropdown"
 				},
 			},
+			extensions = {
+				fzf = {}
+			},
 		})
+
+		require('telescope').load_extension('fzf')
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find words" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-		vim.keymap.set("n", "<leader>fh", function()
-			builtin.find_files({
-				cwd = os.getenv("HOME"),
-			})
-		end, { desc = "Find files in home dir" })
+		vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git commits" })
+		vim.keymap.set("n", "<leader>gg", builtin.git_status, { desc = "Git status" })
 		vim.keymap.set("n", "<leader>fd", function()
 			builtin.find_files({
 				hidden = true,
-				cwd = os.getenv("MY_DOT_FILES"),
+				cwd = os.getenv("DOTFILES"),
 			})
 		end, { desc = "Find config files" })
-		vim.keymap.set("n", "<leader>fn", function()
-			builtin.find_files({
-				cwd = os.getenv("MY_NOTES"),
-			})
-		end, { desc = "Find notes" })
 	end,
 }
